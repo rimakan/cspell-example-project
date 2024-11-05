859e336c16118a7a5797a3ab336fac8889ed19a6
Kirill <k.fedorenko@arenadata.io>
2024-11-05 11:22:39 +0300

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,95 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import cspellPlugin from "@cspell/eslint-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended",
+    )
+  ),
+  {
+    plugins: {
+      react,
+      prettier: fixupPluginRules(prettier),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      "react-hooks": fixupPluginRules(reactHooks),
+      "@cspell": cspellPlugin,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    settings: {},
+
+    rules: {
+      '@cspell/spellchecker': ['warn', {}],
+      "import/no-cycle": [
+        "error",
+        {
+          maxDepth: 10,
+          ignoreExternal: true,
+        },
+      ],
+
+      "no-restricted-imports": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+
+      "object-curly-spacing": [2, "always"],
+      quotes: ["error", "single"],
+      semi: ["warn", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+
+      "space-before-function-paren": [
+        "error",
+        {
+          anonymous: "always",
+          named: "never",
+          asyncArrow: "always",
+        },
+      ],
+
+      "react/jsx-max-props-per-line": [
+        1,
+        {
+          when: "multiline",
+        },
+      ],
+
+      "no-console": [
+        "error",
+        {
+          allow: ["info", "warn", "error"],
+        },
+      ],
+    },
+  },
+];
